1724125c2176f51c07d3951d71f3e9d05a23cd68
nicovanbentum <niico0708@gmail.com>
2018-11-26 11:53:23 +0100

    week2 opgave toegevoegd

---
+++ Week2/objects.cpp
@@ -0,0 +1,96 @@
+#include <SFML/Graphics.hpp>
+#include "objects.hpp"
+
+square::square(sf::Vector2f pos, sf::Vector2f size) :
+	pos(pos),
+	size(size)
+{
+	body.setPosition(pos);
+	body.setSize(size);
+}
+
+void square::move(sf::Vector2f delta)
+{
+	body.setPosition(body.getPosition() + delta);
+}
+
+void square::draw(sf::RenderWindow & window) const 
+{
+	window.draw(body);
+}
+
+walls::walls(sf::Vector2f pos, sf::Vector2f size, int thickness) :
+	pos(pos),
+	size(size),
+	thickness(thickness)
+{
+	top.setPosition(pos);
+	top.setSize(sf::Vector2f(size.x, thickness));
+
+	left.setPosition(sf::Vector2f(pos.x, pos.y + thickness));
+	left.setSize(sf::Vector2f(thickness, size.y));
+
+	right.setPosition(sf::Vector2f((pos.x + size.x) - thickness, pos.y));
+	right.setSize(sf::Vector2f(thickness, size.y));
+
+	bottom.setPosition(sf::Vector2f(pos.x, pos.y + size.y));
+	bottom.setSize(sf::Vector2f(size.x, thickness));
+}
+
+void walls::draw(sf::RenderWindow & window) const
+{
+	window.draw(left);
+	window.draw(right);
+	window.draw(top);
+	window.draw(bottom);
+}
+
+ball::ball( sf::Vector2f position, float size, sf::Vector2f speed ) :
+	position{ position },
+	size{ size },
+	speed{speed}
+{
+	body.setFillColor(sf::Color::Blue);
+	body.setPosition(position);
+	body.setRadius(size);
+}
+
+void ball::draw( sf::RenderWindow & window ) const {
+	window.draw(body);
+}
+
+void ball::move( sf::Vector2f delta ){
+	body.setPosition(body.getPosition() + delta);
+}
+
+void ball::jump( sf::Vector2f target ){
+	position = target;
+}
+
+void ball::jump( sf::Vector2i target ){
+	jump( sf::Vector2f( 
+		static_cast< float >( target.x ), 
+		static_cast< float >( target.y )
+	));
+}
+
+bool ball::overlaps(walls & w)
+{
+
+	if (body.getGlobalBounds().intersects(w.left.getGlobalBounds()) ||
+		body.getGlobalBounds().intersects(w.right.getGlobalBounds()) ||
+		body.getGlobalBounds().intersects(w.top.getGlobalBounds()) ||
+		body.getGlobalBounds().intersects(w.bottom.getGlobalBounds())) {
+		return true;
+	}
+	return false;
+}
+
+bool ball::overlaps(square & s)
+{
+	if (body.getGlobalBounds().intersects(s.body.getGlobalBounds()))
+	{
+		return true;
+	}
+	return false;
+}
---
+++ Week2/objects.hpp
@@ -0,0 +1,68 @@
+#ifndef _BALL_HPP
+#define _BALL_HPP
+
+#include <SFML/Graphics.hpp>
+
+class drawable
+{
+public:
+	virtual void draw(sf::RenderWindow & window) const = 0;
+};
+
+class square : drawable
+{
+public:
+	sf::Vector2f pos;
+	sf::Vector2f size;
+	sf::RectangleShape body;
+
+	square(sf::Vector2f pos, sf::Vector2f size);
+
+	void move(sf::Vector2f delta);
+
+	void draw(sf::RenderWindow & window) const;
+};
+
+class walls : drawable
+{
+public:
+	walls(sf::Vector2f pos, sf::Vector2f size, int thickness = 2);
+
+	void draw(sf::RenderWindow & window) const;
+
+	//variables
+	sf::Vector2f pos;
+	sf::Vector2f size;
+	int thickness;
+
+	sf::RectangleShape left;
+	sf::RectangleShape right;
+	sf::RectangleShape top;
+	sf::RectangleShape bottom;
+};
+
+
+class ball : drawable {
+public:
+
+	ball( sf::Vector2f position, float size, sf::Vector2f speed );
+
+	void draw( sf::RenderWindow & window ) const;
+
+	void move( sf::Vector2f speed );
+
+	void jump( sf::Vector2f target );
+	void jump( sf::Vector2i target );
+
+	bool overlaps(walls & w);
+
+	bool overlaps(square & s);
+
+public:
+	sf::Vector2f position;
+	float size;
+	sf::Vector2f speed;
+	sf::CircleShape body;
+};
+
+#endif
