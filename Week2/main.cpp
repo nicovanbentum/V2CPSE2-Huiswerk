0b5640177912a67a03e1b22574807dc203da234c
nicovanbentum <niico0708@gmail.com>
2018-11-26 15:38:50 +0100

    week 2 opdrachten

--- Week2/main.cpp
+++ Week2/main.cpp
@@ -1,102 +1,103 @@
-#include <iostream>
-#include <functional>
-#include <SFML/Graphics.hpp>
 #include "objects.hpp"
+#include <SFML/Graphics.hpp>
+#include <functional>
+#include <iostream>
 
 class action {
 private:
-	std::function< bool() > condition;
-	std::function< void() > work;
+  std::function<bool()> condition;
+  std::function<void()> work;
+
 public:
-	action(
-		bool b,
-		std::function< void() > work
-	) :
-		condition(
-			[&b]()->bool { return b; }
-		),
-		work(work)
-	{}
+  action(bool b, std::function<void()> work)
+      : condition([&b]() -> bool { return b; }), work(work) {}
 
-	action(
-	   std::function< bool() > condition, 
-	   std::function< void() > work
-	) : condition( condition ), 
-		work( work ) 
-	{}
+  action(std::function<bool()> condition, std::function<void()> work)
+      : condition(condition), work(work) {}
 
-	action(
-		sf::Keyboard::Key key,
-		std::function< void() > work
-	) :
-		condition(
-			[ key ]()->bool { return sf::Keyboard::isKeyPressed( key ); }
-		),
-		work(work)
-	{}
+  action(sf::Keyboard::Key key, std::function<void()> work)
+      : condition([key]() -> bool { return sf::Keyboard::isKeyPressed(key); }),
+        work(work) {}
 
-	action(
-		sf::Mouse::Button button,
-		std::function< void() > work
-	) :
-		condition(
-			[ button ]()->bool { return sf::Mouse::isButtonPressed( button ); }
-		),
-		work(work)
-	{}
+  action(sf::Mouse::Button button, std::function<void()> work)
+      : condition(
+            [button]() -> bool { return sf::Mouse::isButtonPressed(button); }),
+        work(work) {}
 
-	void operator()(){
-		if( condition() ){
-			work();
-		}
-	}
+  void operator()() {
+    if (condition()) {
+      work();
+    }
+  }
 };
 
-int main( int argc, char *argv[] ){
-	std::cout << "Starting application 01-05 array of actions\n";
+int main(int argc, char *argv[]) {
+  std::cout << "Starting application 01-05 array of actions\n";
 
-	sf::RenderWindow window{ sf::VideoMode{ 640, 480 }, "SFML window" };
-	ball my_ball{ sf::Vector2f{ 320.0, 240.0 }, 30, sf::Vector2f(0, 5) };
-	walls my_walls(sf::Vector2f(5, 5), sf::Vector2f(620, 460), 5);
-	square my_square(sf::Vector2f(20, 20), sf::Vector2f(100, 15));
+  sf::RenderWindow window{sf::VideoMode{640, 480}, "SFML window"};
+  ball my_ball{sf::Vector2f{320.0, 240.0}, 30, sf::Vector2f(-5, 5)};
+  walls my_walls(sf::Vector2f(5, 5), sf::Vector2f(620, 460), 10);
 
-	action actions[] = {
-		action([&]()->bool { return my_ball.overlaps(my_walls); }, 
-				[&](){ my_ball.speed.y = my_ball.speed.y * -1; })
-				,
-		action([&]()->bool { return my_ball.overlaps(my_square); }, 
-				[&](){ my_ball.speed.y = my_ball.speed.y * -1; })
-				,
-		action(true, [&]() {my_ball.move(sf::Vector2f(my_ball.speed.x, my_ball.speed.y)); }),
-		action( sf::Keyboard::A,  [&](){ my_square.move( sf::Vector2f( -5.0,  0.0 )); }),
-		action( sf::Keyboard::D, [&](){ my_square.move( sf::Vector2f( +5.0,  0.0 )); }),
-		action( sf::Keyboard::W,    [&](){ my_square.move( sf::Vector2f(  0.0, -5.0 )); }),
-		action( sf::Keyboard::S,  [&](){ my_square.move( sf::Vector2f(  0.0, +5.0 )); }),
-		action( sf::Mouse::Left,     [&](){ my_ball.jump( sf::Mouse::getPosition( window )); })
-	};
+  square my_square(sf::Vector2f(20, 20), sf::Vector2f(100, 15));
 
-	while (window.isOpen()) {
-		for( auto & action : actions ){
-			action();
-		}
+  action actions[] = {
+      action(true, [&]() { my_ball.move(sf::Vector2f(0, my_ball.speed.y)); }),
+      action([&]() -> bool { return my_ball.overlaps(my_square); },
+             [&]() {
+               my_ball.move(sf::Vector2f(0, -1 * my_ball.speed.y));
+               my_ball.speed.y *= -1;
+             }),
+      action([&]() -> bool { return my_ball.overlaps(my_walls); },
+             [&]() {
+               my_ball.move(sf::Vector2f(0, -1 * my_ball.speed.y));
+               my_ball.speed.y *= -1;
+             }),
 
-		window.clear();
-		my_ball.draw( window );
-		my_walls.draw(window);
-		my_square.draw(window);
-		window.display();
+      action(true, [&]() { my_ball.move(sf::Vector2f(my_ball.speed.x, 0)); }),
+      action([&]() -> bool { return my_ball.overlaps(my_square); },
+             [&]() {
+               my_ball.move(sf::Vector2f(-1 * my_ball.speed.x, 0));
+               my_ball.speed.x *= -1;
+             }),
+      action([&]() -> bool { return my_ball.overlaps(my_walls); },
+             [&]() {
+               my_ball.move(sf::Vector2f(-1 * my_ball.speed.x, 0));
+               my_ball.speed.x *= -1;
+             }),
 
-		sf::sleep( sf::milliseconds( 20 ));
+      action(sf::Keyboard::A,
+             [&]() { my_square.move(sf::Vector2f(-5.0, 0.0)); }),
+      action(sf::Keyboard::D,
+             [&]() { my_square.move(sf::Vector2f(+5.0, 0.0)); }),
+      action(sf::Keyboard::W,
+             [&]() { my_square.move(sf::Vector2f(0.0, -5.0)); }),
+      action(sf::Keyboard::S,
+             [&]() { my_square.move(sf::Vector2f(0.0, +5.0)); }),
+      action(sf::Mouse::Left,
+             [&]() { my_square.jump(sf::Mouse::getPosition(window)); })};
 
-        sf::Event event;		
-	    while( window.pollEvent(event) ){
-			if( event.type == sf::Event::Closed ){
-				window.close();
-			}
-		}	
-	}
 
-	std::cout << "Terminating application\n";
-	return 0;
+  while (window.isOpen()) {
+    for (auto &action : actions) {
+      action();
+    }
+
+    window.clear();
+    my_ball.draw(window);
+    my_walls.draw(window);
+    my_square.draw(window);
+    window.display();
+
+    sf::sleep(sf::milliseconds(20));
+
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+    }
+  }
+
+  std::cout << "Terminating application\n";
+  return 0;
 }
-
--- Week2/objects.hpp
+++ Week2/objects.hpp
@@ -3,66 +3,64 @@
 
 #include <SFML/Graphics.hpp>
 
-class drawable
-{
+class drawable {
 public:
-	virtual void draw(sf::RenderWindow & window) const = 0;
+  virtual void draw(sf::RenderWindow &window) const = 0;
 };
 
-class square : drawable
-{
+class square : drawable {
 public:
-	sf::Vector2f pos;
-	sf::Vector2f size;
-	sf::RectangleShape body;
+  sf::Vector2f pos;
+  sf::Vector2f size;
+  sf::RectangleShape body;
 
-	square(sf::Vector2f pos, sf::Vector2f size);
+  square(sf::Vector2f pos, sf::Vector2f size);
 
-	void move(sf::Vector2f delta);
+  void move(sf::Vector2f delta);
 
-	void draw(sf::RenderWindow & window) const;
+  void jump(sf::Vector2f target);
+  void jump(sf::Vector2i target);
+
+  void draw(sf::RenderWindow &window) const;
 };
 
-class walls : drawable
-{
+class walls : drawable {
 public:
-	walls(sf::Vector2f pos, sf::Vector2f size, int thickness = 2);
+  walls(sf::Vector2f pos, sf::Vector2f size, int thickness = 2);
 
-	void draw(sf::RenderWindow & window) const;
+  void draw(sf::RenderWindow &window) const;
 
-	//variables
-	sf::Vector2f pos;
-	sf::Vector2f size;
-	int thickness;
+  // variables
+  sf::Vector2f pos;
+  sf::Vector2f size;
+  int thickness;
 
-	sf::RectangleShape left;
-	sf::RectangleShape right;
-	sf::RectangleShape top;
-	sf::RectangleShape bottom;
+  sf::RectangleShape left;
+  sf::RectangleShape right;
+  sf::RectangleShape top;
+  sf::RectangleShape bottom;
 };
-
 
 class ball : drawable {
 public:
+  ball(sf::Vector2f position, float size, sf::Vector2f speed);
 
-	ball( sf::Vector2f position, float size, sf::Vector2f speed );
+  void draw(sf::RenderWindow &window) const;
 
-	void draw( sf::RenderWindow & window ) const;
+  void move(sf::Vector2f speed);
 
-	void move( sf::Vector2f speed );
+  void jump(sf::Vector2f target);
+  void jump(sf::Vector2i target);
 
-	void jump( sf::Vector2f target );
-	void jump( sf::Vector2i target );
+  bool overlaps(walls &w);
 
-	bool overlaps(walls & w);
-
-	bool overlaps(square & s);
+  bool overlaps(square &s);
 
 public:
-	sf::Vector2f position;
-	float size;
-	sf::Vector2f speed;
-	sf::CircleShape body;
+  sf::Vector2f position;
+  float size;
+  sf::Vector2f speed;
+  sf::CircleShape body;
 };
 
 #endif
